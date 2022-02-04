0a60276b2f941fac6f70db0dbfec37b79da011c0
khankaev <akhankaev@gmail.com>
2022-02-04 09:58:11 +0200

    commit

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"simulator/pkg/result"
 )
 
@@ -26,8 +25,7 @@ func Server() {
 	if port == "" {
 		port = "8383" // Default port if not specified
 	}
-	fs := http.FileServer(neuteredFileSystem{http.Dir("./web")})
-	r.Handle("/web/", http.StripPrefix("/web", fs))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
 	r.HandleFunc("/api", handleConnection)
 	s := &http.Server{
 		Addr:    ":" + port,
@@ -45,28 +43,4 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	w.Write(res)
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "status_page.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-	return f, nil
 }
