434e18eeb914e149ab1794256716cfab21f127f2
khankaev <akhankaev@gmail.com>
2022-02-09 14:21:07 +0200

    commit

--- cmd/simulator/main.go
+++ cmd/simulator/main.go
@@ -18,7 +18,7 @@ func main() {
 		port = "8383"
 	}
 	ticker := time.NewTicker(30 * time.Second)
-	res := result.GetRes()
+	var res result.ResultT
 	go func() {
 		for range ticker.C {
 			emulator.Shuffle()
